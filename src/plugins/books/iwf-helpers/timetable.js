a7c8e2804730fac5d43346ee7d52777330ca9585
Jean-François Lamy <jf@jflamy.dev>
2026-01-14 21:46:14 -0500

    Replace books subtree with tracker-books submodule

--- src/plugins/books/iwf-helpers/timetable.js
+++
@@ -1,6 +0,0 @@
-/**
- * Timetable building helpers
- * Re-exports from session-helpers for backward compatibility
- */
-
-export { buildTimetableData, formatSessionTime } from './session-helpers.js';
